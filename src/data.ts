3a6f6146edd420207b02d8578e2420b9e97dcc94
MarceloRavache <jm.ravache@outlook.com>
2021-07-24 22:10:46 -0300

    add: data of products

---
+++ src/data.ts
@@ -0,0 +1,27 @@
+
+const data = () => {
+    const products = [
+        {
+            id: 1,
+            image: 'https://rduirapuru.com.br/wp-content/uploads/2020/01/geladeiras-1024x768.jpg',
+            title: 'Geladeira',
+            description: 'Otima para quem está começando a fazer a sua cozinha',
+        },
+        {
+            id: 2,
+            image: 'https://www.lg.com/us/images/tvs/md06065036/gallery/OLED77W9PUA_1100_v2.jpg',
+            title: 'Tv',
+            description: 'Otima para quem está começando a fazer a sua casa',
+        },
+        {
+            id: 3,
+            image: 'https://pallomaristore.com/wp-content/uploads/2021/05/echo-dot-3-design.jpg',
+            title: 'Echo Dot',
+            description: 'Otima para quem está começando a fazer a sua casa',
+        }
+    ]
+
+    return { products}
+}
+
+export default data;
